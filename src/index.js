8466e697242540abe6d576955bf14723e32653d4
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-25 22:18:51 +0530

    changed to react 17

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 
 // Render whole app
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
