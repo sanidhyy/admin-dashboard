3fc744adc592bd5cc1f1e1a59982ae2c4ed3576a
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-26 17:45:08 +0530

    context created

--- src/App.js
+++ src/App.js
@@ -22,10 +22,12 @@ import {
   ColorMapping,
   Editor,
 } from "./pages";
+import { useStateContext } from "./contexts/ContextProvider";
+
 import "./App.css";
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
@@ -43,7 +45,7 @@ const App = () => {
             </TooltipComponent>
           </div>
           {activeMenu ? (
-            <div className="w-72 fixed-sidebar dark:bg-secondary-dark-bg bg-white">
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
               <Sidebar />
             </div>
           ) : (
