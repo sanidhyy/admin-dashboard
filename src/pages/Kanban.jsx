3261804ff87e76f28c0b73dd439b2ff6381d2372
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-28 22:05:48 +0530

    kanban added

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  KanbanComponent,
+  ColumnsDirective,
+  ColumnDirective,
+} from "@syncfusion/ej2-react-kanban";
+
+import { kanbanData, kanbanGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Kanban = () => {
-  return <div>Kanban</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Kanban" />
+      <KanbanComponent
+        id="kanban"
+        dataSource={kanbanData}
+        cardSettings={{ contentField: "Summary", headerField: "Id" }}
+        keyField="Status"
+      >
+        <ColumnsDirective>
+          {kanbanGrid.map((item, i) => (
+            <ColumnDirective key={i} {...item} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
+  );
 };
 
 export default Kanban;
