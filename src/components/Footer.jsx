59068bc862405bce005bca0372565ba37c9ce8a4
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-29 23:26:53 +0530

    new icon added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { MdOutlineCopyright } from "react-icons/md";
 
 const Footer = () => (
   <div className="mt-24">
     <p className="dark:text-gray-200 text-gray-700 text-center m-20">
-      © 2022 All rights reserved by Shoppy.com
+      <MdOutlineCopyright /> 2022 All rights reserved by Shoppy.com
     </p>
   </div>
 );
