ee787049825b803ac55f1ac727fe3cb7e8925f5c
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-30 15:49:51 +0530

    fixed footer typos + dates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { MdOutlineCopyright } from "react-icons/md";
 
 const Footer = () => (
   <div className="mt-24">
     <p className="dark:text-gray-200 text-gray-700 text-center m-20">
-      <MdOutlineCopyright /> 2022 All rights reserved by Shoppy.com
+      &copy; {new Date().getFullYear()} All rights reserved by Shoppy.
     </p>
   </div>
 );
